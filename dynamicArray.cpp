7d42a34168311fd29b2d7d55418a34ae499398e6
shehzad1995 <34193598+shehzad1995@users.noreply.github.com>
2021-01-06 19:34:38 +0100

    Update and rename DynamischesArray.cpp to dynamicArray.cpp

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -10,43 +10,43 @@ version: Alpha 0.1
 using namespace std;
 
 struct Array {
-	float* data;
-	int size;
+	float* data; //attribute is a pointer for values in the heap section
+	int size; //attribute will contain the size of the array
 	Array create(int size) {
 		Array a;
-		a.data = new float[size];
+		a.data = new float[size]; //reserves space in the heap section
 		a.size = size;
 		return a;
 	}
 	void destroy(Array& a) {
-		delete[] a.data;
+		delete[] a.data; //sets reserved space free
 		a.data = nullptr;
 		a.size = 0;
 	}
 	void print() {
 		for (int i = 0; i < size; i++)
-			cout << *(data + i) << " ";
+			cout << *(data + i) << " "; //outputs every element of the array
 		cout << endl;
 	}
 	void fillRandom() {
 		srand(time(nullptr));
 		for (int i = 0; i < size; i++)
-			*(data + i) = rand();
+			*(data + i) = rand(); //fills array with random values
 	}
 	void copy(Array& b) {
 		if (size > b.size) {
-			Array c = c.create(size);
+			Array c = c.create(size); //creates new array if there is not enough space in the array where the values have to be copied
 			for (int i = 0; i < b.size; i++)
 				*(c.data + i) = *(b.data + i);
 			destroy(b);
 			b = c;
 		}
 		for (int i = 0; i < size; i++)
-			*(b.data + i) = *(data + i);
+			*(b.data + i) = *(data + i); //copies values into the target array
 	}
 	void resize(int aSize) {
 		if (aSize < size)
-			return;
+			return; //quits function if the new size is smaller than the current size of the array
 		Array a = a.create(aSize);
 		copy(a);
 		destroy(*this);
